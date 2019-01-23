0d479d18ac397db0b393c919102ca68b5b17600e
Heoliny Jung <ojung@iu.edu>
2019-01-22 23:35:01 -0500

    Completed hw2 (Assignment2).

--- exercise/hw2/Nim.c
+++ exercise/hw2/Nim.c
@@ -1,4 +1,5 @@
-/* Heoliny Jung
+/* 
+ * Heoliny Jung
  * C291
  * Prof. Bhutta
  * Assignment2: Nim Game
@@ -7,33 +8,67 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
+
+//class variables
+int compBrain;
 
 //Function declarations
-in randLim(int limit);
+int randLim(int);
+int compTurn(int);
+int playerTurn(int);
 
 int main(void){
 
  srand(time(0)); //sets pseduorandom seed for optimal randomization
 
  int pileSize = randLim(90)+10; //90 = limit, + 10 for 10-100
- int whoGoesFirst = randLim(1); //0-1
- int compBrain = randLim(1); //0-1
+ int whosTurn = 0; //randLim(1); 0 = comp, 1 = player
+ compBrain = 0;//randLim(1); 0-1, 0 = stupid, 1 = smart
 
- while(pileSize>0){
-  if(whoGoesFirst==0){
-   whoGoesFirst = 1;
+ if(whosTurn){
+  printf("You go first!\n");
+ }
+ else{
+  printf("Computer goes first!\n");
+ }
+
+ if(compBrain){
+  printf("The computer is smart! Good luck.\n");
+ }
+ else{
+  printf("The computer is stupid!\n");
+ }
+
+ printf("The pile size is %d\n",pileSize);
+
+ while(pileSize>1){
+  int amt = 0;
+  if(whosTurn==0){
+   amt = compTurn(pileSize);
+   printf("The computer removed %d from the pile.\n",amt);
+   whosTurn = 1;
   }
   else{
-   whoGoesFirst = 0;
+   amt = playerTurn(pileSize);
+   printf("You removed %d form the pile.\n",amt);
+   whosTurn = 0;
   }
-  
+  pileSize-=amt;
+  printf("The remaining amount in the pile is %d\n",pileSize);
+ }
+ if(whosTurn == 1){
+  printf("Computer wins!\n"); 
+ }
+ else{
+  printf("You win!\n");
  }
 
  return 0;
 }
 
-/* Returns a random number between 0 and limit inclusive
- * 
+/* 
+ * Returns a random number between 0 and limit inclusive
  * Example found online as how to distribute rand ints more evenly than rand()%range
  */
 int randLim(int limit){
@@ -46,4 +81,42 @@ int randLim(int limit){
  } while (retval > limit);
 
  return retval;
+}
+
+/* 
+ * Player's turn function
+ * Scans input from user and returns that int if within bounds of 0<int<pileSize/2
+ */
+int playerTurn(int pileSize){
+ int input = 0;
+ 
+ while(1){
+  printf("Enter amount to remove: ");
+  scanf("%d",&input);
+  if(input<1||input>pileSize/2){
+   printf("Not a valid amount.\n");
+  }
+  else{
+   return input;
+  }
+ }
+}
+
+/*
+ * Computer's turn function
+ * Using rules 0 = stupid, 1 = smart, will return amount taken for the computer's turn.
+ */
+int compTurn(int pileSize){
+ int amt = 0;
+ if(compBrain == 1){
+  double n = 0;
+  while(amt<pileSize/2){
+   amt = pow(2.0,n++)-1;
+  }
+ }
+ else{
+  int lim = (pileSize/2);
+  amt = randLim((lim)-1)+1;
+ }
+ return amt;
 }
---
+++ exercise/hw2/ShapeDrawIf.c
@@ -0,0 +1,144 @@
+/*
+ * Heoliny Jung
+ * C291
+ * Prof Adeel Bhutta
+ * Assignment 2: Shape Draw with if-else statements
+ */
+
+#include<stdio.h>
+
+//Function declarations
+void squareDraw(int);
+void diamondDraw(int);
+void triangleDraw(int);
+
+int main(void){
+ int quit = 1;
+ while(quit){
+  
+  printf("Choose a shape to display:\n1. Square (s)\n2. Diamond (d)\n3. Triangle (t)\nQuit (q)\n");
+  char choice;
+  scanf("%c",&choice);
+
+  if(choice=='q'||choice=='Q'){
+   quit = 0;
+  }
+  else if(choice=='s'||choice=='S'||choice=='d'||choice=='D'||choice=='t'||choice=='T'){
+   int h;
+   do{
+    printf("Enter a height (7-17): ");
+    scanf("%d",&h);
+    if(h<7||h>17){
+     printf("Not a valid height.\n");
+    }
+   }while(h<7||h>17);
+
+   if(choice=='s'||choice=='S'){
+    squareDraw(h);
+   }
+ 
+   else if(choice=='d'||choice=='D'){
+    diamondDraw(h);
+   }
+ 
+   else if(choice=='t'||choice=='T'){
+    triangleDraw(h);
+   }
+   
+   else{
+    printf("Not a valid input.\n");
+   }
+  }
+ }
+
+ return 0;
+}
+
+void squareDraw(int h){
+ int i;
+ for(i = 0; i<h; i++){
+  int l = h-1;
+  while(l>=0){
+   if(i == 0||i == h-1){
+    printf("*");
+   }
+   else if(l == 0||l == h-1){
+    printf("*");
+   }
+   else{
+    printf(" ");
+   }
+   l--;
+  }
+  printf("\n");
+ }
+}
+
+void diamondDraw(int h){
+ int i;
+ int mid = h/2;
+ int left = mid;
+ int right = mid;
+ for(i = 0; i<h; i++){
+  int l = h-1;
+  while(l>=0){
+   if(i == 0||i == h-1){
+    if(l == mid){
+     printf("*");
+    }
+    else{
+     printf(" ");
+    }
+   }
+   else if(l == left||l == right){
+    printf("*");
+   }
+   else{
+    printf(" ");
+   }
+   l--;
+  }
+  if(i+1>mid){
+   left--;
+   right++;
+  }
+  else{
+   left++;
+   right--;
+  }
+  printf("\n");
+ }
+}
+
+void triangleDraw(int h){
+int i;
+ int mid = h/2;
+ int left = mid;
+ int right = mid;
+ for(i = 0; i<mid; i++){
+  int l = h+1;
+  while(l>0){
+   if(i == 0){
+    if(l == mid){
+     printf("*");
+    }
+    else{
+     printf(" ");
+    }
+   }
+   else if(l == left||l == right){
+    printf("*");
+   }
+   else if(i == mid-1){
+    printf("*");
+   }
+   else{
+    printf(" ");
+   }
+   l--;
+  }
+  left--;
+  right++;
+  printf("\n");
+ }
+}
